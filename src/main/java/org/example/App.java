6baf1455cad558c4a661e4a647de226ea6329eff
Nathan Derhore <nathan@derhore.fr>
2023-05-02 19:32:27 +0200

    implements first part of game

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -7,15 +10,26 @@ package org.example;
 public class App 
 {
 
+
+    //jeu de données
+     static final String[] sentences = {"norme","ordinateur","voiture","java","canard"};
+     static final int essai = 6;
+
+    static Scanner sc = new Scanner(System.in);
+
     public static void main( String[] args )
     {
 
         System.out.println( "Bienvenue dans notre jeu du pendu !" );
+        String saisie = selectChoice();
 
-
-        //menu : jouer ou quitter
-
-        //getSentence : genere un mot
+        switch (saisie){
+            case "b":
+                launchGame();
+                break;
+            case "x":
+                System.exit(0);
+        }
 
         //InputWord : lecture clavier sur la lettre saisie
 
@@ -24,6 +38,28 @@ public class App
         //DisplaySetence : affiche la phrase avec les lettres trouvées , tata => _a_a
     }
 
+     static String selectChoice() {
+        String saisie = "";
+
+        while(!(saisie.equals("b") || saisie.equals("x"))) {
+            System.out.println("Entrez b pour commencer, x pour quitter.");
+            saisie = sc.nextLine();
+        }
+        return saisie;
+    }
+
+    static void launchGame() {
+        System.out.println("Debut du jeu !");
+        System.out.println("Generation d'un mot ...");
+        String sentence = getSentence();
+
+
+    }
+
+     static String getSentence() {
+        Random random = new Random();
+        return sentences[random.nextInt(sentences.length)];
+    }
 
 
 }
