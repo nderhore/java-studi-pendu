9e75eadb6594fac0aaca3714e01a603b8f27dd8f
Nathan Derhore <nathan@derhore.fr>
2023-05-02 19:06:59 +0200

    init project

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,8 +6,24 @@ package org.example;
  */
 public class App 
 {
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        System.out.println( "Bienvenue dans notre jeu du pendu !" );
+
+
+        //menu : jouer ou quitter
+
+        //getSentence : genere un mot
+
+        //InputWord : lecture clavier sur la lettre saisie
+
+        //CalculateTry : incrémente un compteur avec les essais. Bien sur, l'essai est en dur
+
+        //DisplaySetence : affiche la phrase avec les lettres trouvées , tata => _a_a
     }
+
+
+
 }
