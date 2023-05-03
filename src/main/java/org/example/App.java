e1fe83454bdfd3e02ce7d8ce6cddc6e897b831b3
Nathan Derhore <nathan@derhore.fr>
2023-05-03 19:05:41 +0200

    implements pendu game

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -13,7 +14,13 @@ public class App
 
     //jeu de données
      static final String[] sentences = {"norme","ordinateur","voiture","java","canard"};
-     static final int essai = 6;
+
+
+     static final int countTry = 6;
+
+     static int essai = 0;
+
+
 
     static Scanner sc = new Scanner(System.in);
 
@@ -31,9 +38,6 @@ public class App
                 System.exit(0);
         }
 
-        //InputWord : lecture clavier sur la lettre saisie
-
-        //CalculateTry : incrémente un compteur avec les essais. Bien sur, l'essai est en dur
 
         //DisplaySetence : affiche la phrase avec les lettres trouvées , tata => _a_a
     }
@@ -50,13 +54,63 @@ public class App
 
     static void launchGame() {
         System.out.println("Debut du jeu !");
-        System.out.println("Generation d'un mot ...");
-        String sentence = getSentence();
 
+
+        System.out.println("Generation d'un mot ...");
+
+        //recuperation de la phrase
+        String sentence = getSentence();
+        String[] sentenceWithSecret = initializeSentenceWithSecret(sentence);
+
+
+        int essai = 0;
+        boolean found = false;
+
+        while(essai < countTry && !found){
+            System.out.println("voici l'etat actuel de la recherche : " + displaySentenceWithSecret(sentenceWithSecret));
+            getAndModifySentenceWithSecret(sentenceWithSecret, sentence);
+            found = checkIfSentenceFounded(sentenceWithSecret);
+
+        }
 
     }
 
-     static String getSentence() {
+    private static String displaySentenceWithSecret(String[] sentenceWithSecret) {
+        String stringToDisplay = "";
+        for(int i = 0; i<sentenceWithSecret.length; i++){
+            stringToDisplay += sentenceWithSecret[i];
+        }
+        return stringToDisplay;
+    }
+
+    private static boolean checkIfSentenceFounded(String[] sentenceWithSecret) {
+        return Arrays.stream(sentenceWithSecret).noneMatch(caractere -> caractere.equals("_"));
+    }
+
+    private static void getAndModifySentenceWithSecret(String[] sentenceWithSecret, String sentence) {
+        System.out.println("Entrez un caractère : ");
+        String saisie = sc.nextLine();
+        if(!sentence.contains(saisie)){
+            essai++;
+            System.out.println("la lettre " + saisie + " n'est pas dans le mot.");
+        }
+        else {
+            for(int i = 0; i<sentence.length(); i++){
+                if(sentence.charAt(i) == saisie.charAt(0)) sentenceWithSecret[i] = saisie;
+            }
+        }
+
+    }
+
+    private static String[] initializeSentenceWithSecret(String sentence) {
+        String[] sentenceSecret = new String[sentence.length()];
+        Arrays.fill(sentenceSecret,"_");
+        return sentenceSecret;
+    }
+
+
+
+    static String getSentence() {
         Random random = new Random();
         return sentences[random.nextInt(sentences.length)];
     }
